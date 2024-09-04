d34e8c99edc8f0ac9e7d29a52e7026da481c4854
Jpedr0098 <joao.maurano.ismart@gmail.com>
2024-09-04 00:21:10 -0300

    Copiando os dois ultimos exercicios OBS Não sei pq ta dando erro...

--- Aula_28-08/ex5.ts
+++ Aula_28-08/ex5.ts
@@ -1,10 +1,9 @@
 function medirTempoDeExecucao(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-
     const metodoOriginal = descriptor.value;
 
     descriptor.value = function (...args: any[]) {
         console.time(propertyKey);  
-        const resultado = metodoOriginal.apply(this, args); 
+        const resultado = metodoOriginal.apply(this, args);  
         console.timeEnd(propertyKey);  
         return resultado;
     };
@@ -13,9 +12,7 @@ function medirTempoDeExecucao(target: any, propertyKey: string, descriptor: Prop
 }
 
 class Calculadora {
-    @medirTempoDeExecucao
     somarNumeros(array: number[]): number {
-
         return array.reduce((a, b) => a + b, 0); 
     }
 }
